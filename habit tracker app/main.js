3b6c9010f9182fca29fb80f1a9ba4439006e5882
kirabo22 <lunyoloevenylkirabo20@gmail.com>
2025-03-05 09:16:24 +0300

    i figured i was not getting the website to look like i wanted it to look like

--- habit tracker app/main.js
+++ habit tracker app/main.js
@@ -3,9 +3,8 @@ const habits =document.querySelectorAll('.habit-btn');
 
 habits.forEach(habit => {
     habit.addEventListener('click', ()=> {
-        console.log('clicked');//#endregion
         habit.classList.toggle('completed');
         
-    })
+    });
 
-    })
+    });
